ce4cd51a622bb4b7b963005441311ee9d5ad1bfd
mjoelbreen <mjoelbreen@gmail.com>
2022-09-10 12:09:03 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Martins-Website`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
